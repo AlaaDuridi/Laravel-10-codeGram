7fd5dbbb074c242fb52ba935f9f3389001f16a1f
Alaa.N.Duridi <alla_duridi@hotmail.com>
2023-12-02 02:34:25 +0200

    CRUD with raw sql, and query builder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,74 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*tried by me : alaa duridi*/ 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    //**************raw sql queries *******************
+
+    //try dd , dump and die 
+
+    //1- fetch all users 
+    // $users = DB::select('select * from users ');
+
+    //2-insert user into users table 
+    // $user = DB::insert('insert into users (name, email, password) values (?,?,?)', [
+    //     'alaa',
+    //     'alaa.duridi7@gmail.com',
+    //     '188271020027265'
+    // ]);
+
+    //3- update user 
+    // $user = DB::update('update users set email = ? where id = ?', [
+    //     'rahaf.com', 3
+    // ]);
+
+    //4- delete a user 
+    // $user = DB::delete('delete from users where id = ?', [1]);
+
+    //**********query builders *************
+
+    //get specific user 
+    // $users = DB::table('users')->where('name', 'alaa')->get();
+    //get all users 
+    // $users = DB::table('users')->get(); ##################Main#######################
+    // $users = DB::table('users')->where('name', 'Hiba')->first();
+    // $users = DB::table('users')->where('id', 5)->first();
+    // the line ubove this line is the same as the one below it 
+    $users = DB::table('users')->find(5); /// SO POWERFULL 
+    //insert a user 
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Hiba',
+    //     'email' => 'Hiba@s',
+    //     'password' => ''
+    // ]);
+
+    //update a user 
+    // $user = DB::table('users')->where('id', 5)->update([
+    //     'email' => 'Hiba@Salamah'
+    // ]); will return 1 or 0 
+
+    //insert another user 
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Romaysa',
+    //     'email' => 'Romy.Sis',
+    //     'password' => ''
+    // ]); // will return true or false 
+
+    //delete a user 
+    // $user = DB::table('users')->where('name', 'Romaysa')->delete();
+
+    dd($users); ##########Main#######################################
 });
 
-// // Route::view('/','home');
-// Route::view('/','welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
